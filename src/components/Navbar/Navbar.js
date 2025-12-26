53e4f6cd9d8fce61d5ba85dd23c69d2fd6f5b169
unknown <nandodivinodev@gmail.com>
2025-12-26 19:28:11 -0300

    criei a pasta navbar, nela eu criei o arquivo Navbar(para o menu de navegação, o campo de pesquisa dos produtos e o carrinho de produtos) e o arquivo Navbar.scss(para personalizar o navbar)

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,32 @@
+import './Navbar.scss';
+import { useState } from 'react';
+
+export function Navbar({ search, setSearch }) {
+    const [icone, setIcone] = useState('fa-magnifying-glass');
+    const [active, setActive] = useState(false);
+
+
+    function handleClick() {
+        setActive(!active);
+        setIcone(active ? 'fa-magnifying-glass' : 'fa-chevron-down');
+    }
+    return (
+        <nav className='p-6'>
+
+            <button onClick={handleClick}>
+                <i className={"fa-solid " + icone}></i>
+            </button>
+
+            <input
+                type='text'
+                placeholder='o que você procura?'
+                className={`${active ? 'hidden' : 'block'} p-3`}
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
+            />
+            <button className='btn-cart'>
+                <i className={"fa-solid fa-cart-shopping"}></i>
+            </button>
+        </nav>
+    )
+}
