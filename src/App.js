85ee0382c6e6201de8befb4c53021955e1f31e37
unknown <nandodivinodev@gmail.com>
2025-12-26 19:19:56 -0300

    criei uma pequena logica para que conseguisse compartilhar os estados dos arquivos atravez de props e assim conseguisse criar o filtro de pesquisa comparrtilhando os dados entre o navbar, card e useproducts(que recebe os dados da api)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
+import { useState } from "react";
 import { Card } from "./components/Card/Card";
+import { useProducts } from "./services/useProducts";
+import { Navbar } from "./components/Navbar/Navbar";
+
 
 function App() {
+  const { products, loading } = useProducts();
+  const [search, setSearch] = useState('');
+
+  const filteredProducts = products.filter((product) => (
+    product.title.toLowerCase().includes(search.toLowerCase())
+  ));
+
   return (
     <div>
-      <h1> E-commerce App</h1>
-      <Card />
+      <Navbar search={search} setSearch={setSearch} />
+
+      <main>
+        {loading ? <p>Carregando...</p> :
+          filteredProducts.map((product) => (
+            <Card key={product.id} filteredProducts={product} />
+          ))
+        }
+      </main>
+
     </div>
   );
 }
