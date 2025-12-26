13aebf16260e9f666ed041358d4bed3d332262e1
unknown <nandodivinodev@gmail.com>
2025-12-26 19:21:35 -0300

    alterei a estrutura dos dados recebidos e por isso tive que alterar alguns elementos para o funcionamento do card, dessa forma alterei o estilo visto que alguns elementos foram removidos

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,19 +1,14 @@
 import './Card.scss';
-import {useProducts} from '../../services/useProducts';
+export function Card({ filteredProducts }) {
 
-export function Card() {
-    const { products } = useProducts();
     return (
-        <div className="h-screen card-container">
-            <ul className='w-full py-9 flex flex-wrap justify-center items-center gap-8'>
-                {products.map((product)=>(
-                    <div className='w-screen mx-6 flex flex-col items-center justify-center'>
-                        <li key={product.id}><img src={product.image} alt={product.title} className='mt-4'/></li>
-                        <li key={product.id}>{product.title}</li>
-                        <li key={product.id}>{product.price} R$</li>
-                    </div>
-                ))}
-            </ul>
+        <div className="card flex flex-col gap-4 m-4 p-4">
+            <div className=' flex flex-col items-center justify-center p-4 '>
+                <img src={filteredProducts.image} alt={filteredProducts.title} />
+                <h2>{filteredProducts.title}</h2>
+                <span>{(filteredProducts.price + 20.00).toFixed(2)} R$</span>
+                <p>{(filteredProducts.price).toFixed(2)} R$</p>
+            </div>
         </div>
     )
 }
