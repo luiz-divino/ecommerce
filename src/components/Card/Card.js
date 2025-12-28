9ef9781c195c5c643c6f254cf70a00d225fd6485
unknown <nandodivinodev@gmail.com>
2025-12-27 21:43:54 -0300

    fiz ajustes de estilo dos cards para diminuir o tamanho das imagens e limitar o comprimento dos titulos dos produtos

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -2,10 +2,10 @@ import './_card.scss';
 export function Card({ filteredProducts }) {
 
     return (
-        <div className="card flex flex-col gap-4 m-4 p-4">
-            <div className=' flex flex-col items-center justify-center p-4 '>
+        <div className="card m-4  ">
+            <div className='flex flex-col items-center justify-center'>
                 <img src={filteredProducts.image} alt={filteredProducts.title} />
-                <h2>{filteredProducts.title}</h2>
+                <h2 className='line-clamp-2'>{filteredProducts.title}</h2>
                 <span>{(filteredProducts.price + 20.00).toFixed(2)} R$</span>
                 <p>{(filteredProducts.price).toFixed(2)} R$</p>
             </div>
